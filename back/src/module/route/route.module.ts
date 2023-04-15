59848883077eedffa8d2b60b75a4f9c84eec1493
이동원 <dongwon@unitsoft.kr>
2023-04-15 21:47:53 +0900

    init

--- back/src/module/route/route.module.ts
+++ back/src/module/route/route.module.ts
@@ -1,15 +1,18 @@
-import { Module } from "@nestjs/common";
-import { RouteService } from "./route.service";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { RouteEntity } from "@modules/route/entity";
+import { Module } from '@nestjs/common';
+import { RouteService } from './route.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RouteEntity } from '@modules/route/entity';
 
-import { RouteResolver } from "@modules/route/resolvers";
-import { RoleRouteMapEntity } from "@modules/role/entity";
-import { TypeOrmExModule } from "@common/module/TypeOrmExModule";
-import { RouteRepository } from "@modules/route/repositories";
-import { RoleRepository, RoleRouteMapRepository } from "@modules/role/repository";
+import { RouteResolver } from '@modules/route/resolvers';
+import { RoleRouteMapEntity } from '@modules/role/entity';
+import { TypeOrmExModule } from '@common/module/TypeOrmExModule';
+import { RouteRepository } from '@modules/route/repositories';
+import {
+  RoleRepository,
+  RoleRouteMapRepository,
+} from '@modules/role/repository';
 
-@Module({f
+@Module({
   imports: [
     TypeOrmModule.forFeature([RouteEntity, RoleRouteMapEntity]),
     TypeOrmExModule.forCustomRepository([
